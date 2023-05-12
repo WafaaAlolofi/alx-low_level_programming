73b7e2f59eeab067ed2826ba954480fce93a81e3
WafaaAlolofi <wa.alolofi@gmail.com>
2023-05-12 14:09:47 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
 * @argc: number of arguments
 * @argv: array of arguments
 **
-Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 
-printf("%s\n", *argv); // it is the same argv[0]
+printf("%s\n", argv[0]); // it is the same *argv
 
 return (0);
 
